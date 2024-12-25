40271de744b2a7a2a8f31678262385ea7d01af9b
saeed khorsand <saeed.mkhorsand@gmail.com>
2024-12-25 18:46:12 +0330

    Create ApplicationDbContext.cs

---
+++ Core/Context/ApplicationDbContext.cs
@@ -0,0 +1,33 @@
+namespace AspnetCoreMvcFull.Core.Context;
+
+using AspnetCoreMvcFull.Models.User;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System.Text.RegularExpressions;
+
+public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+{
+  public DbSet<UserGroup> Groups { get; set; }
+  //public DbSet<Product> Products { get; set; }
+
+  public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+  protected override void OnModelCreating(ModelBuilder builder)
+  {
+    base.OnModelCreating(builder);
+
+    // Seed Data
+    builder.Entity<UserGroup>().HasData(
+        new UserGroup { Id = 1, Name = "شیر" },
+        new UserGroup { Id = 2, Name = "ماست" }
+    );
+
+    /*    builder.Entity<Product>().HasData(
+            new Product { Id = 1, Name = "شیر کم‌چرب", GroupId = 1 },
+            new Product { Id = 2, Name = "شیر پرچرب", GroupId = 1 },
+            new Product { Id = 3, Name = "ماست ساده", GroupId = 2 },
+            new Product { Id = 4, Name = "ماست میوه‌ای", GroupId = 2 },
+            new Product { Id = 5, Name = "ماست یونانی", GroupId = 2 }
+        );*/
+  }
+}
