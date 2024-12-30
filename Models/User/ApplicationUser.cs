c8e73ac74b63398786885e08187a9ab970ac4361
saeed khorsand <saeed.mkhorsand@gmail.com>
2024-12-30 13:00:23 +0330

    Update ApplicationUser.cs

--- Models/User/ApplicationUser.cs
+++ Models/User/ApplicationUser.cs
@@ -1,10 +1,12 @@
 namespace AspnetCoreMvcFull.Models.User;
 
 using Microsoft.AspNetCore.Identity;
+using System.Text.RegularExpressions;
 
 public class ApplicationUser : IdentityUser
 {
   public string Gender { get; set; } // جنسیت
-  public int GroupId { get; set; }  // گروه (کلید خارجی)
-  public UserGroup Group { get; set; }  // رابطه با جدول گروه‌ها
+  public int GroupId { get; set; } // کلید خارجی گروه
+  public UserGroup Group { get; set; } // رابطه با جدول گروه
+  public ICollection<Consumption> Consumptions { get; set; } // تاریخچه مصرف
 }
