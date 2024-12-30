418faef04d0df79cc209fa4f4ec938cb16f93364
saeed khorsand <saeed.mkhorsand@gmail.com>
2024-12-30 13:00:26 +0330

    Create AddConsumptionRequest.cs

---
+++ Models/ViewModels/AddConsumptionRequest.cs
@@ -0,0 +1,10 @@
+namespace AspnetCoreMvcFull.Models.ViewModels
+{
+  public class AddConsumptionRequest
+  {
+    public int UserId { get; set; }
+    public int? ProductId { get; set; }
+    public DateTime ConsumptionTime { get; set; }
+    public double Quantity { get; set; }
+  }
+}
