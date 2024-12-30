9654e992838c329b9dbd503cc6b980c31c2f99e1
saeed khorsand <saeed.mkhorsand@gmail.com>
2024-12-30 13:00:28 +0330

    Create UpdateConsumptionRequest.cs

---
+++ Models/ViewModels/UpdateConsumptionRequest.cs
@@ -0,0 +1,8 @@
+namespace AspnetCoreMvcFull.Models.ViewModels
+{
+  public class UpdateConsumptionRequest
+  {
+    public DateTime ConsumptionTime { get; set; }
+    public double Quantity { get; set; }
+  }
+}
