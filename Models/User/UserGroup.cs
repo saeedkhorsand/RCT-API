d3ae8d686d3aed819ae1383e9eb45390c30139c8
saeed khorsand <saeed.mkhorsand@gmail.com>
2024-12-30 13:00:21 +0330

    Update UserGroup.cs

--- Models/User/UserGroup.cs
+++ Models/User/UserGroup.cs
@@ -3,4 +3,6 @@ public class UserGroup
 {
   public int Id { get; set; }
   public string Name { get; set; }
+  public ICollection<ApplicationUser> Users { get; set; } // کاربران مرتبط با این گروه
+  public ICollection<Product> Products { get; set; } // محصولات مرتبط (مثلاً انواع شیر یا ماست)
 }
