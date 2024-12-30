fb66eadc43ca3518ac04f0cd78b1a2f140bdf66d
saeed khorsand <saeed.mkhorsand@gmail.com>
2024-12-30 12:59:56 +0330

    Create Consumption.cs

---
+++ Models/Consumption.cs
@@ -0,0 +1,15 @@
+using AspnetCoreMvcFull.Models.User;
+
+namespace AspnetCoreMvcFull.Models
+{
+  public class Consumption
+  {
+    public int Id { get; set; }
+    public int UserId { get; set; } // کلید خارجی کاربر
+    public ApplicationUser User { get; set; } // رابطه با کاربر
+    public int ProductId { get; set; } // کلید خارجی محصول
+    public Product Product { get; set; } // رابطه با محصول
+    public DateTime ConsumptionTime { get; set; } // تاریخ و ساعت مصرف
+    public double Quantity { get; set; } // مقدار مصرف (مثلاً به میلی‌لیتر یا گرم)
+  }
+}
