f3e0a5b51598142663b026f8552a6741bd9648a8
saeed khorsand <saeed.mkhorsand@gmail.com>
2025-01-08 00:37:52 +0330

    Update LoginRequest.cs

--- Models/ViewModels/LoginRequest.cs
+++ Models/ViewModels/LoginRequest.cs
@@ -1,8 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace AspnetCoreMvcFull.Models.ViewModels
 {
   public class LoginRequest
   {
+    [Required]
     public string Username { get; set; }
+    [Required]
     public string Password { get; set; }
   }
 }
