b142eb87f364a826617392a5506cd8afb7a86c1a
saeed khorsand <saeed.mkhorsand@gmail.com>
2025-01-08 00:37:46 +0330

    Update ApplicationUser.cs

--- Models/User/ApplicationUser.cs
+++ Models/User/ApplicationUser.cs
@@ -5,8 +5,8 @@ using System.Text.RegularExpressions;
 
 public class ApplicationUser : IdentityUser
 {
-  public string Gender { get; set; } // جنسیت
-  public int GroupId { get; set; } // کلید خارجی گروه
-  public UserGroup Group { get; set; } // رابطه با جدول گروه
+  public string? Gender { get; set; } // جنسیت
+  public int? GroupId { get; set; } // کلید خارجی گروه
+  public UserGroup? Group { get; set; } // رابطه با جدول گروه
   public ICollection<Consumption> Consumptions { get; set; } // تاریخچه مصرف
 }
