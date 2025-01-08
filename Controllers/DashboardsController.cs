b47b835c88893d58cf7d4571d043784e0e5631b2
saeed khorsand <saeed.mkhorsand@gmail.com>
2025-01-08 12:42:03 +0330

    Update DashboardsController.cs

--- Controllers/DashboardsController.cs
+++ Controllers/DashboardsController.cs
@@ -1,10 +1,17 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using AspnetCoreMvcFull.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authentication.Cookies;
 
 namespace AspnetCoreMvcFull.Controllers;
 
+[Authorize]
 public class DashboardsController : Controller
 {
-  public IActionResult Index() => View();
+  public IActionResult Index()
+  {
+    return View();
+  }
 }
