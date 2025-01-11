0fb957e525b1582e2c79190e6ae0a93948ab869c
saeed khorsand <saeed.mkhorsand@gmail.com>
2025-01-11 16:37:48 +0330

    Create 20250111065417_ConsumptionDsc.cs

---
+++ Migrations/20250111065417_ConsumptionDsc.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AspnetCoreMvcFull.Migrations
+{
+    /// <inheritdoc />
+    public partial class ConsumptionDsc : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Dsc",
+                table: "Consumptions",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Dsc",
+                table: "Consumptions");
+        }
+    }
+}
