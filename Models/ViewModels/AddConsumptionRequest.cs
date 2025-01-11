e096d0817e7cc16f2bb3348495dbc1cef10ec485
saeed khorsand <saeed.mkhorsand@gmail.com>
2025-01-11 16:37:58 +0330

    Update AddConsumptionRequest.cs

--- Models/ViewModels/AddConsumptionRequest.cs
+++ Models/ViewModels/AddConsumptionRequest.cs
@@ -6,5 +6,6 @@ namespace AspnetCoreMvcFull.Models.ViewModels
     public int? ProductId { get; set; }
     public DateTime ConsumptionTime { get; set; }
     public double Quantity { get; set; }
+    public string? Dsc { get; set; }
   }
 }
